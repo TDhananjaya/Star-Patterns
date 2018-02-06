62bcf7e29bef356d370ee33603b1963da26e434d
TDhananjaya <tharindudhananjaya1122@gmail.com>
2018-02-06 15:57:57 +0530

    Star Pattern Sample 2

---
+++ starpattern2.java
@@ -0,0 +1,15 @@
+public class StarPatterns1 {
+ 
+     public static void main(String[] args) {
+       int i, j, k=1;
+        for(i=0; i<5; i++)
+        {
+            for(j=0; j<k; j++)
+            {
+                System.out.print("* ");
+            }
+            k = k + 2;
+            System.out.println();
+        }
+    }
+}
