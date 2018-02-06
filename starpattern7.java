12abe34ed10a842c463739e49099e62f294b271c
TDhananjaya <tharindudhananjaya1122@gmail.com>
2018-02-07 00:21:52 +0530

    Star Pattern Sample 7

---
+++ starpattern7.java
@@ -0,0 +1,14 @@
+public class starpattern7{
+
+       public static void main(String args[]) {
+        int i, j, n = 1;
+        for (i = 0; i < 5; i++) {
+            for (j = 0; j <= i; j++) {
+                System.out.print(n + " ");
+                n++;
+            
+            }
+            System.out.println();
+        }
+    }
+}
