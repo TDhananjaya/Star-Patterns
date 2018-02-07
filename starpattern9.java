f179af5bb8fee3ee5f1b3e69f7317b6f7c16a960
TDhananjaya <tharindudhananjaya1122@gmail.com>
2018-02-07 16:40:21 +0530

    Star Pattern Sample 9

---
+++ starpattern9.java
@@ -0,0 +1,16 @@
+public class starpattern8{
+
+       public static void main(String args[]) {
+         {
+            int i, j, num;
+            for (i = 0; i < 5; i++) {
+                num = 1;
+                for (j = 0; j <= i; j++) {
+                    System.out.print(num + " ");
+                    num++;
+                }
+                System.out.println();
+            }
+        }
+    }
+}
